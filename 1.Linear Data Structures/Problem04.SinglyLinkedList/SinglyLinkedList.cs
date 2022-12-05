4fec5fd9c8fc0c2024be4fbf10c2630c04cf0296
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-12-05 22:45:19 +0200

    Implementing of linked list structure completed

--- 1.Linear Data Structures/Problem04.SinglyLinkedList/SinglyLinkedList.cs
+++ 1.Linear Data Structures/Problem04.SinglyLinkedList/SinglyLinkedList.cs
@@ -6,46 +6,154 @@
 
     public class SinglyLinkedList<T> : IAbstractLinkedList<T>
     {
-        public int Count => throw new NotImplementedException();
+        private Node top;
+
+        private class Node
+        {
+            private T value;
+            private Node next;
+
+            public T Value { get => this.value; set => this.value = value; }
+            public Node Next { get => this.next; set => this.next = value; }
+
+            public Node(T value)
+            {
+                this.Value = value;
+                this.Next = null;
+            }
+
+
+            public Node(T value, Node next)
+            {
+                this.Value = value;
+                this.Next = next;
+            }
+
+        }
+
+        public int Count { get; set; }
 
         public void AddFirst(T item)
         {
-            throw new NotImplementedException();
+            Node newNode = new Node(item);
+            if(this.top == null)
+            {
+                top = newNode;
+            }
+            else
+            {
+                Node oldNode = this.top;
+                this.top = newNode;
+                newNode.Next = oldNode;
+            }
+            this.Count++;
         }
 
         public void AddLast(T item)
         {
-            throw new NotImplementedException();
+            Node newNode = new Node(item);
+            if (this.top == null)
+            {
+                top = newNode;
+            }
+            else
+            {
+                Node currentNode = this.top;
+                while(currentNode.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+                currentNode.Next = newNode;
+            }
+            this.Count++;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            Node currentNode = this.top;
+            while(currentNode != null)
+            {
+                yield return currentNode.Value;
+                currentNode = currentNode.Next;
+            }
         }
 
         public T GetFirst()
         {
-            throw new NotImplementedException();
+            if(this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                return this.top.Value;
+            }
         }
 
         public T GetLast()
         {
-            throw new NotImplementedException();
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                Node currentNode = this.top;
+                while(currentNode.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+                return currentNode.Value;
+            }
         }
 
         public T RemoveFirst()
         {
-            throw new NotImplementedException();
+            if(this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                Node oldNode = this.top;
+                this.top = oldNode.Next;
+                this.Count--;
+                return oldNode.Value;
+            }
+           
         }
 
         public T RemoveLast()
         {
-            throw new NotImplementedException();
+
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            else if(this.Count == 1)
+            {
+                Node nodeToDelete = this.top;
+                this.top = null;
+                this.Count--;
+                return nodeToDelete.Value;
+
+            }
+            else
+            {
+                Node currentNode = this.top;
+                while ( currentNode.Next.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+                Node nodeToDelete = currentNode.Next;
+                currentNode.Next = null;
+                this.Count--;
+                return nodeToDelete.Value;
+            }
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+       
+
     }
 }
