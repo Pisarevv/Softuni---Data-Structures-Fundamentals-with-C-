eb23f95c5413fb9566b118636fbf23d8bd9be1ca
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-12-05 21:22:39 +0200

    Stack structure implemented successfully

--- 1.Linear Data Structures/Problem02.Stack/Stack.cs
+++ 1.Linear Data Structures/Problem02.Stack/Stack.cs
@@ -8,41 +8,125 @@
     {
         private class Node
         {
+            private T value;
+            private Node next;
+
+            public T Value { get => this.value; set => this.value = value; }
+            public Node Next { get => this.next; set => this.next = value; }
+
+            public Node(T value)
+            {
+                this.Value = value; 
+                this.Next = null;
+            }
+
+
+            public Node(T value, Node next)
+            {
+                this.Value = value;
+                this.Next = next;
+            }
             
         }
 
         private Node top;
 
-        public int Count => throw new System.NotImplementedException();
+        public int Count { get; set; }
 
         public void Push(T item)
         {
-            throw new NotImplementedException();
+            Node newNode = new Node(item);
+            if(this.top == null)
+            {
+                this.top = newNode;
+            }
+            else
+            {
+                var oldNode = top;
+                this.top = newNode;
+                this.top.Next = oldNode;
+            }
+            this.Count++;
         }
 
         public T Pop()
         {
-            throw new System.NotImplementedException();
+            if(this.Count > 0)
+            {
+                var oldTop = top;
+                this.top = oldTop.Next;
+                this.Count--;
+                return oldTop.Value;
+            }
+            else
+            {
+                throw new InvalidOperationException("Structure is empty");
+            }
+            
         }
 
         public T Peek()
         {
-            throw new System.NotImplementedException();
+            if(this.Count > 0)
+            {
+                return this.top.Value;
+            }
+            else
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+           
         }
 
         public bool Contains(T item)
         {
-            throw new System.NotImplementedException();
+            if(this.Count > 0)
+            {
+                var currentNode = this.top;
+                if(this.Count == 1)
+                {
+                    if (item.Equals(currentNode.Value))
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    while (currentNode.Next != null)
+                    {
+                        if (item.Equals(currentNode.Value))
+                        {
+                            return true;
+                        }
+                        currentNode = currentNode.Next;
+                    }
+                }
+               
+            }
+
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var currentNode = this.top;
+            while (true)
+            {
+                if(currentNode.Next != null)
+                {
+                    yield return currentNode.Value;
+                    currentNode = currentNode.Next;
+                }
+                else
+                {
+                    yield return currentNode.Value;
+                    break;
+                }
+             
+            }
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+       
     }
 }
