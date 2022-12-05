7185613fa02cf099af4f61daf2cb7091fef5d2e6
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-12-05 21:48:52 +0200

    Queue structure implemented successfully

--- 1.Linear Data Structures/Problem03.Queue/Queue.cs
+++ 1.Linear Data Structures/Problem03.Queue/Queue.cs
@@ -8,41 +8,104 @@
     {
         private class Node
         {
-            
+            private T value;
+            private Node next;
+
+            public T Value { get => this.value; set => this.value = value; }
+            public Node Next { get => this.next; set => this.next = value; }
+
+            public Node(T value)
+            {
+                this.Value = value;
+                this.Next = null;
+            }
+
+
+            public Node(T value, Node next)
+            {
+                this.Value = value;
+                this.Next = next;
+            }
+
         }
 
         private Node head;
 
-        public int Count => throw new NotImplementedException();
+        public int Count { get; set; }
 
         public void Enqueue(T item)
         {
-            throw new NotImplementedException();
+            Node newNode = new Node(item);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node currentNode = head;
+                while (currentNode.Next != null)
+                {
+                    currentNode = currentNode.Next;
+                }
+                currentNode.Next = newNode;
+            }
+            this.Count++;
+          
         }
 
         public T Dequeue()
         {
-            throw new NotImplementedException();
+            if(Count > 0)
+            {
+                Node oldHead = head;
+                head = oldHead.Next;
+                this.Count--;
+                return oldHead.Value;
+            }
+            else
+            {
+                throw new InvalidOperationException("Queue is empty!");
+            }
+            
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            if (Count > 0)
+            {      
+                return head.Value;
+            }
+            else
+            {
+                throw new InvalidOperationException("Queue is empty!");
+            }
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            var node = this.head;
+            while(node != null)
+            {
+                if (item.Equals(node.Value))
+                {
+                    return true;
+                }
+                node = node.Next;
+            }
+            return false;
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            var node = this.head;
+            while(node != null)
+            {
+                yield return node.Value;
+                node = node.Next;
+            }
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        
     }
 }
