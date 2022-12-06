f779124b4804ba60239fc964a2507b563c617032
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-12-06 22:22:30 +0200

    Task of testing balanced parentheses completed successfully

---
+++ 1.Linear Data Structures - Exercise/04.BalancedParentheses/BalancedParenthesesSolve.cs
@@ -0,0 +1,47 @@
+﻿namespace Problem04.BalancedParentheses
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BalancedParenthesesSolve : ISolvable
+    {
+        public bool AreBalanced(string parentheses)
+        {
+            if(parentheses.Length % 2 != 0)
+            {
+                return false;
+            }
+
+            var stack = new Stack<char>();
+            foreach(char currCh in parentheses)
+            {
+                char expectedChar = default;
+                switch (currCh)
+                {
+                    case ')':
+                        expectedChar = '(';
+                        break;
+                    case ']':
+                        expectedChar = '[';
+                        break;
+                    case '}':
+                        expectedChar = '{';
+                        break;
+                    default:
+                        stack.Push(currCh);
+                    break;
+
+                }
+                if(expectedChar == default)
+                {
+                    continue;
+                }
+                if(stack.Pop() != expectedChar)
+                {
+                    return false;
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
