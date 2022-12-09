9ccf98c56e4512e36c45e9814fc11dda63d79d67
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-12-09 12:20:58 +0200

    Tree structure, bfs and dfs algorithms implemented

---
+++ 2.Trees Representation and Traversal (BFS, DFS)/Tree/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var tree = new Tree<int>(34,
+                           new Tree<int>(36,
+                             new Tree<int>(42),
+                             new Tree<int>(3,
+                                    new Tree<int>(78))
+                             ),
+                           new Tree<int>(1),
+                           new Tree<int>(103)
+                           );
+
+             Console.WriteLine(string.Join((", "), tree.OrderDfs()));
+        }
+    }
+}
---
+++ 2.Trees Representation and Traversal (BFS, DFS)/Tree/Tree.cs
@@ -0,0 +1,98 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private List<Tree<T>> children;
+        private T value;
+        private Tree<T> parent;
+
+        public Tree(T value)
+        {
+            this.value = value;
+            this.children = new List<Tree<T>>();
+        }
+
+        public Tree(T value, params Tree<T>[] children)
+            : this(value)
+        {
+            this.value=value;
+            foreach( Tree<T> child in children)
+            {
+                child.parent = this;
+                this.children.Add(child);
+
+            }
+        }
+
+        public void AddChild(T parentKey, Tree<T> child)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> OrderBfs()
+        {
+            var result = new List<T>();
+            var queue = new Queue<Tree<T>>();
+            queue.Enqueue(this);
+            while (queue.Count > 0)
+            {
+                var currNode = queue.Dequeue();
+                result.Add(currNode.value);
+                foreach(var child in currNode.children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+            return result;
+
+            
+        }
+
+        private void Dfs(Tree<T> node, ICollection<T> result)
+        {
+            foreach(var child in node.children)
+            {
+                this.Dfs(child, result);
+            }
+            result.Add(node.value);
+        }
+
+        public IEnumerable<T> OrderDfs()
+        {
+           var list = new List<T>();
+           this.Dfs(this, list);
+           return list;
+        }
+
+        private IEnumerable<T> DfsWithStack()
+        {
+            var result = new Stack<T>();
+            var stack = new Stack<Tree<T>>();
+            stack.Push(this);
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+                foreach (var child in node.children)
+                {
+                    stack.Push(child);
+                }
+                result.Push(node.value);
+            }
+
+            return result;
+        }
+
+        public void RemoveNode(T nodeKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Swap(T firstKey, T secondKey)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
