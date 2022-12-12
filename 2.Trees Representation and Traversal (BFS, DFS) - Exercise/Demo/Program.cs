0441ca6c97bdaa3f9e59f63296842b54646c3260
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-12-12 23:32:22 +0200

    Tree class update

--- 2.Trees Representation and Traversal (BFS, DFS) - Exercise/Demo/Program.cs
+++ 2.Trees Representation and Traversal (BFS, DFS) - Exercise/Demo/Program.cs
@@ -7,7 +7,11 @@
     {
         static void Main(string[] args)
         {
-            
+            var input = new string[] { "9 17", "9 4", "9 14", "4 36", "14 53", "14 59", "53 67", "53 73" };
+            var treeFactory = new IntegerTreeFactory();
+
+            var tree = treeFactory.CreateTreeFromStrings(input);
+            Console.WriteLine(tree);
         }
     }
 }
