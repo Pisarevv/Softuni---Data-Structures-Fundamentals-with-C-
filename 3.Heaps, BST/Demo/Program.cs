66c34dc78bb636f4474092991ba99c00aea99112
Hristo Pisarev <hristo.pisarev98@gmail.com>
2022-12-14 21:40:24 +0200

    Binary tree traversal methods implemented

---
+++ 3.Heaps, BST/Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using _02.BinarySearchTree;
+using _03.MaxHeap;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 9;
+            Console.Write(Fib(n));
+        }
+
+        public static int Fib(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+        }
+    }
+
+}
